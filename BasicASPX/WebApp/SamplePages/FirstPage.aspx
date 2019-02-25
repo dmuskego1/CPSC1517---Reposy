c98ed5abaf374a734008e346a261b784d3dede3f
dmuskego1 <46543403+dmuskego1@users.noreply.github.com>
2019-02-25 16:00:33 -0700

    ASP Introduction

---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FirstPage.aspx.cs" Inherits="WebApp.SamplePages.FirstPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Enter your name"></asp:Label>
+    &nbsp&nbsp
+    <asp:TextBox ID="YourName" runat="server"></asp:TextBox>
+    <br/>
+    <asp:Button ID="PressMe" runat="server" Text="Press Me" />
+    <br />
+    <asp:Literal ID="Output" runat="server"></asp:Literal>
+</asp:Content>
---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class FirstPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //This method is executed on Each and every pass of the Page
+            //This Method is executed before any of your event methods
+
+        }
+        protected void PressMe_Click()
+        {
+            Output.Text = "Hello There " + YourName.Text;
+        }
+    }
+}
